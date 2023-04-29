26a7c8edffd9d6553f6cdcd0f89564de1a047160
Refilwe Mafalo <11824@ctucareer.co.za>
2023-04-29 12:59:19 +0200

    Doe522

---
+++ DOE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DOE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there!");
+        }
+    }
+}
